d2ccf884f924b630103c5e95b119e82a71f0d83e
YJBeetle <YJBeetle@gmail.com>
2016-02-26 03:59:10 +0800

    解决字体。实现了居中等。。参数还需斟酌

--- draw.c
+++ draw.c
@@ -10,6 +10,19 @@
 #include "draw.h"
 
 
+struct color color_code2rgba(int32_t code)
+{
+    struct color argb;
+    if(code<=0xffffff)
+        argb.alpha=(double)1;
+    else
+        argb.alpha=(double)(code>>24&0xff)/(double)0xff;
+    argb.red=   (double)(code>>16&0xff)/(double)0xff;
+    argb.green= (double)(code>>8&0xff)/(double)0xff;
+    argb.blue=  (double)(code&0xff)/(double)0xff;
+    return argb;
+}
+
 void drawsvg (cairo_t *cr, char *svgfilename, double x, double y, double width, double height)
 {
     FILE *svgfile;
@@ -32,7 +45,6 @@ void drawsvg (cairo_t *cr, char *svgfilename, double x, double y, double width, 
     //设置绘画位置大小
     cairo_translate (cr, x, y);
     svg_cairo_get_size (svgc, &svg_width, &svg_height);//取得svg大小
-printf("svg:[%s] size:[%d],[%d]\n",svgfilename,svg_width,svg_height);
     scaleX=width/(double)svg_width;
     scaleY=height/(double)svg_height;
     cairo_scale (cr, scaleX, scaleY);
@@ -45,31 +57,25 @@ printf("svg:[%s] size:[%d],[%d]\n",svgfilename,svg_width,svg_height);
     cairo_restore(cr);//还原画笔
 }
 
-void drawtext (cairo_t *cr,char *text, double x, double y)
+void drawtext (cairo_t *cr,char *text, char *family, double font_size, int32_t color_code, double x, double y)
 {
     cairo_save(cr);//保存画笔
-    cairo_move_to (cr, 40, 40); /* Bottom left of text at point */
-    cairo_set_source_rgba (cr, 0, 0, 0, 1);
-    cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-    cairo_set_font_size (cr, 10);
-    cairo_show_text (cr, text);
+    struct color argb=color_code2rgba(color_code);
+    cairo_set_source_rgba (cr, argb.red, argb.green, argb.blue, argb.alpha);
+    cairo_select_font_face (cr, family, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size (cr, font_size);
+
+    cairo_text_extents_t extents;
+    cairo_text_extents(cr,text,&extents);
+    cairo_move_to (cr, x-extents.width/2, y);
+
+    //cairo_show_text (cr, text);
+    cairo_text_path (cr, text);
+    cairo_fill(cr);
     cairo_restore(cr);//还原画笔
 }
 
-struct color color_code2rgba(int code)
-{
-    struct color argb;
-    if(code<=0xffffff)
-        argb.alpha=(double)1;
-    else
-        argb.alpha=(double)(code>>24&0xff)/(double)0xff;
-    argb.red=   (double)(code>>16&0xff)/(double)0xff;
-    argb.green= (double)(code>>8&0xff)/(double)0xff;
-    argb.blue=  (double)(code&0xff)/(double)0xff;
-    return argb;
-}
-
-void draw_rectangle(cairo_t *cr, int color_code, double x, double y, double width, double height)
+void draw_rectangle(cairo_t *cr, int32_t color_code, double x, double y, double width, double height)
 {
     cairo_save(cr);//保存画笔
     struct color argb=color_code2rgba(color_code);
@@ -110,7 +116,10 @@ void draw (char *outfile)
     draw_rectangle(cr, 0x686767, 0, 0, page_width, 16);
     draw_rectangle(cr, 0x686767, 0, page_height-16, page_width, 16);
     drawsvg(cr,"logo.svg", (page_width-50)/2, 30, 50, 37.544);
-    drawtext(cr,"YJBeetle", 0, 0);
+    drawtext(cr,"Add:No.9 West Section of the South 2nd Ring.LianHu District.Xi'an", "Lantinghei SC Demibold", 5, 0xffffff, page_width/2, 10);
+    drawtext(cr,"Add:No.9 West Section of the South 2nd Ring.LianHu District.Xi'an", "Lantinghei SC Demibold", 5, 0xffffff, page_width/2, page_height-6);
+    drawtext(cr,"YJBeetle", "Yuanti SC", 20, 0x686767, page_width/2, 100);
+    drawtext(cr,"A测试中文", "Lantinghei SC Extralight", 20, 0x686767, page_width/2, 120);
 
     cairo_destroy (cr);//回收画笔
     cairo_surface_destroy (surface);//回收介质
