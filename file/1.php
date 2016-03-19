868f357fef5d58732eb416b3f18742e432368e8d
YJBeetle <YJBeetle@gmail.com>
2016-03-20 00:09:37 +0800

    add file/

---
+++ file/1.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+$data=array(
+	(object)["type"=>"rectangle","color_code"=>"FCF7E8"],
+	(object)["type"=>"svg","filename"=>"bg-veins.svg"],
+	(object)["type"=>"png","filename"=>"1.png"],
+	(object)["type"=>"text","text"=>"YJBeetle"]
+);
+echo "=========\n";
+echo json_encode($data)."\n";
+echo "=========\n";
+
