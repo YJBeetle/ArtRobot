85afebd585ac9dc89961eaeef2ca5ccb59b53078
YJBeetle <YJBeetle@YJBeetle-MP.lan>
2016-04-04 22:15:18 +0800

    添加class json

--- draw.h
+++ draw.h
@@ -6,6 +6,8 @@ class draw
 private:
     int8_t inited;
 
+    json json;
+
     //const char *outfile;
     //const char *out_type;
     double page_width;
@@ -16,20 +18,6 @@ private:
 
     int8_t init(const char *filename,const char *type,double width,double height);
     int8_t uninit();
-
-    int8_t jsondata_inited;
-    JsonParser *jsondata_parser;
-    JsonNode *jsondata_node;
-    JsonReader *jsondata_reader;
-    int8_t jsondata_init(const char *jsondata);
-    int32_t jsondata_count();
-    int8_t jsondata_read_member(const char *member);
-    int8_t jsondata_end_member();
-    int8_t jsondata_read_element(int32_t i);
-    int8_t jsondata_end_element();
-    const char * jsondata_get_string(const char *item);
-    int64_t jsondata_get_int(const char *item);
-    double jsondata_get_double(const char *item);
 
     int8_t draw_rectangle(color argb, double x, double y, double width, double height);
     int8_t draw_text(const char *text, const char *family, double font_size, int8_t alignment, color argb, double x, double y);
