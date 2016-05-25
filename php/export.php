cec9a4c676b7473a47911c33ef26d578b44c17c6
俞亦键 <yjbeetle@gmail.com>
2016-05-25 21:29:37 +0800

    分离了设置页面和执行页面 实现了根据设置页面来设置实际文字

---
+++ php/export.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YJBeetle
+ * Date: 16/5/25
+ * Time: 下午4:33
+ */
+
+
+//选中模板
+$template='1';
+
+//设置工作目录
+$dir="template/".$template;
+chdir($dir);
+
+//读取json文件
+$filename="data.json";
+$file = fopen($filename, "r") or die("Unable to open file!");
+$jsontext=fread($file,filesize($filename));
+fclose($file);
+
+//数据处理
+$json=json_decode($jsontext);
+$outfile=sys_get_temp_dir().'/out.svg';
+$json->outfile=$outfile;
+
+//设置文字内容
+$json->draw[7]->text=$_GET['text'];
+$json->draw[8]->text=$_GET['text2'];
+$json->draw[9]->text='会议时间'.$_GET['date'];
+
+//编码并写入json文件
+$jsonrun=json_encode($json);
+$filename=sys_get_temp_dir().'/run.json';
+$file = fopen($filename, "w") or die("Unable to open file!");
+fwrite($file,$jsonrun);
+fclose($file);
+
+
+//run
+$cmdline=dirname(__FILE__).'/exec/Art_robot -f "'.sys_get_temp_dir().'/run.json'.'"';
+$return=exec($cmdline);
+
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Art robot测试页面</title>
+    <style type="text/css">
+        #textarea {
+            width: 100%;
+        }
+        #textarea2 {
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+<h1>Art robot测试页面</h1>
+<h3>输出预览</h3>
+<p><img src="imgshow.php"></p>
+<h3>调试输出</h3>
+<p>
+    模板json数据
+    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsontext;?></textarea>
+    处理后数据结构
+    <textarea name="textarea" rows="10" id="textarea"><?php echo print_r($json);?></textarea>
+    生成json数据
+    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsonrun;?></textarea>
+    command line
+    <textarea name="textarea" rows="10" id="textarea"><?php echo $cmdline;?></textarea>
+    <br>
+    stdout
+    <textarea name="textarea2" rows="5" id="textarea2"><?php echo $return;?></textarea>
+</p>
+</body>
+</html>
--- php/index.php
+++ php/index.php
@@ -1,75 +1,20 @@
-<?php
-//选中模板
-$template='1';
-
-//设置工作目录
-$dir="template/".$template;
-chdir($dir);
-
-//读取json文件
-$filename="data.json";
-$file = fopen($filename, "r") or die("Unable to open file!");
-$jsontext=fread($file,filesize($filename));
-fclose($file);
-
-//数据处理
-$json=json_decode($jsontext);
-$outfile=sys_get_temp_dir().'/out.svg';
-$json->outfile=$outfile;
-$json->draw[8]->text="word啊";
-
-//编码写入
-$jsonrun=json_encode($json);
-$filename=sys_get_temp_dir().'/run.json';
-$file = fopen($filename, "w") or die("Unable to open file!");
-fwrite($file,$jsonrun);
-fclose($file);
-
-
-//run
-$cmdline=dirname(__FILE__).'/exec/Art_robot -f "'.sys_get_temp_dir().'/run.json'.'"';
-$return=exec($cmdline);
-
-
-
-?>
-
-
-
 <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Art robot测试页面</title>
-    <style type="text/css">
-        #textarea {
-            width: 100%;
-        }
-        #textarea2 {
-            width: 100%;
-        }
-    </style>
 </head>
 
 <body>
-<h1>Art robot测试页面</h1>
-<h3>设置</h3>
-<p>&nbsp;</p>
-<h3>输出预览</h3>
-<p><img src="imgshow.php"></p>
-<h3>调试输出</h3>
-<p>
-    模板json数据
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsontext;?></textarea>
-    处理后数据结构
-    <textarea name="textarea" rows="10" id="textarea"><?php echo print_r($json);?></textarea>
-    生成json数据
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsonrun;?></textarea>
-    command line
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $cmdline;?></textarea>
-    <br>
-    stdout
-    <textarea name="textarea2" rows="5" id="textarea2"><?php echo $return;?></textarea>
-</p>
+<h1>Art robot测试页面-商务水牌</h1>
+<form name="form" method="GET" enctype="multipart/form-data" action="export.php" >
+    <h3>设置</h3>
+    <p>标题:<input type="text" name="text" value="会议名称"></p>
+    <p>标题:<input type="text" name="text2" value="会议名称第二行"></p>
+    <p>会议时间:<input type="datetime-local" name="date" value="2016-01-01T00:00:00"></p>
+    <p>会议地点:<input type="text" name="where" value="会议室1"></p>
+    <p>会议地点（英文）:<input type="text" name="where-en" value="room 1"></p>
+    <p><button type="submit">提交</button></p>
+</form>
 </body>
 </html>
