839c638968e223508d5eedba1b6d0bb2b9ce7869
俞亦键 <yjbeetle@gmail.com>
2016-05-26 19:20:48 +0800

    去除了一个变量使用 应该可以节约内存吧?

--- php/imgshow.php
+++ php/imgshow.php
@@ -4,7 +4,5 @@ header('Content-type: image/svg+xml');
 $filename=sys_get_temp_dir().'/out.svg';
 
 $file = fopen($filename, "rb");
-$svg=fread($file,filesize($filename));
+echo fread($file,filesize($filename));
 fclose($file);
-
-echo $svg;
