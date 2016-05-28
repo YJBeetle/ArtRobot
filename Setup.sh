30474a5a24c4e86b983dacde715a82851782fe1f
YJBeetle <YJBeetle@gmail.com>
2016-05-29 03:09:02 +0800

    大幅度调整了目录结构，

--- Setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir build
-mkdir php/exec
-ln -s ../../build/Art_robot php/exec/Art_robot
-mkdir php/tmp
-chmod 777 php/tmp
--- Start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo killall php-fpm
-sudo nginx -s stop
-sudo php-fpm&
-#sudo nginx -s reload
-sudo nginx
