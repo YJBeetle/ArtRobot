ba60f7090ebab364da23bb6bddfcd3f7abe124c7
YJBeetle <YJBeetle@gmail.com>
2016-05-29 05:38:42 +0800

    网页大更新，export.php直接读入json并生成，输出。

--- export.php
+++ export.php
@@ -5,89 +5,41 @@
  * Date: 16/5/25
  * Time: 下午4:33
  */
+//init
+session_start();
 
-//常量
-//$TMPDIR=sys_get_temp_dir();
 $TMPDIR=dirname(__FILE__).'/tmp';
-if(!is_dir($TMPDIR))mkdir($TMPDIR,0777);
+$outfile=$TMPDIR.'/exportdata';
+
+//获取数据
+//$jsondata=$GLOBALS['HTTP_RAW_POST_DATA'];
+$jsondata=$_SESSION['jsondata'];
 
 //选中模板
-$template='1';
+$template = '1';
 
 //设置工作目录
-$dir="template/".$template;
+$dir = "template/" . $template;
 chdir($dir);
 
-//读取json文件
-$filename="data.json";
-$file = fopen($filename, "r") or die("Unable to open file!");
-$jsontext=fread($file,filesize($filename));
-fclose($file);
-
 //数据处理
-$json=json_decode($jsontext);
-$outfile=$TMPDIR.'/out.svg';
-$json->outfile=$outfile;
-
-//设置文字内容
-$json->draw[0][7]->text=$_GET['text'];
-$json->draw[0][8]->text=$_GET['text2'];
-$json->draw[0][9]->text='会议时间:'.$_GET['date'];
-$json->draw[0][10]->text='会议地点:'.$_GET['where'];
-$json->draw[0][11]->text='Date:'.$_GET['date-en'];
-$json->draw[0][12]->text='Location:'.$_GET['where-en'];
+$data=json_decode($jsondata);
+$data->outfile=$outfile;
 
 //编码并写入json文件
-$jsonrun=json_encode($json);
+$jsonrun=json_encode($data);
 $filename=$TMPDIR.'/run.json';
 $file = fopen($filename, "w") or die("Unable to open file!");
 fwrite($file,$jsonrun);
 fclose($file);
 
-
 //run
-$cmdline=dirname(__FILE__).'/exec/Art_robot -f "'.$TMPDIR.'/run.json'.'" 2>&1';
-//$return=exec($cmdline);
+$cmdline=dirname(__FILE__).'/exec/Art_robot -f "'.$TMPDIR.'/run.json'.'" ';
+exec($cmdline);
+//passthru($cmdline);
 
+header('Content-type: image/svg+xml');
 
-?>
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Art robot测试页面</title>
-    <style type="text/css">
-        #textarea {
-            width: 100%;
-        }
-        #textarea2 {
-            width: 100%;
-        }
-    </style>
-</head>
-
-<body>
-<h1>Art robot测试页面</h1>
-<h3>输出预览</h3>
-<p><img src="imgshow.php"></p>
-<h3>调试输出</h3>
-<p>
-    模板json数据
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsontext;?></textarea>
-    处理后数据结构
-    <textarea name="textarea" rows="10" id="textarea"><?php echo print_r($json);?></textarea>
-    生成json数据
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $jsonrun;?></textarea>
-    command line
-    <textarea name="textarea" rows="10" id="textarea"><?php echo $cmdline;?></textarea>
-    <br>
-    stdout
-    <textarea name="textarea2" rows="5" id="textarea2"><?php
-        //passthru('env');
-        //passthru('export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin');
-        //passthru('export DYLD_LIBRARY_PATH=/opt/local/lib/');
-        passthru($cmdline);
-        ?></textarea>
-</p>
-</body>
-</html>
+$file = fopen($outfile, "rb");
+echo fread($file,filesize($outfile));
+fclose($file);
--- index.php
+++ index.php
@@ -1,13 +1,61 @@
+<?php
+//init
+session_start();
+
+if(@$_POST['submit']) {
+//常量
+//$TMPDIR=sys_get_temp_dir();
+    $TMPDIR = dirname(__FILE__) . '/tmp';
+    if (!is_dir($TMPDIR)) mkdir($TMPDIR, 0777);
+
+//选中模板
+    $template = '1';
+
+//设置工作目录
+    $dir = "template/" . $template;
+    chdir($dir);
+
+//读取json文件
+    $filename = "data.json";
+    $file = fopen($filename, "r") or die("Unable to open file!");
+    $jsontext = fread($file, filesize($filename));
+    fclose($file);
+
+//数据处理
+    $json = json_decode($jsontext);
+
+//设置文字内容
+    $json->draw[0][7]->text = $_POST['text'];
+    $json->draw[0][8]->text = $_POST['text2'];
+    $json->draw[0][9]->text = '会议时间:' . $_POST['date'];
+    $json->draw[0][10]->text = '会议地点:' . $_POST['where'];
+    $json->draw[0][11]->text = 'Date:' . $_POST['date-en'];
+    $json->draw[0][12]->text = 'Location:' . $_POST['where-en'];
+
+//编码并写入
+    $jsonrun = json_encode($json);
+    $_SESSION['jsondata']=$jsonrun;
+}
+?>
 <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Art robot测试页面</title>
+
+    <style type="text/css">
+        #textarea {
+            width: 100%;
+        }
+        #textarea2 {
+            width: 100%;
+        }
+    </style>
 </head>
 
 <body>
 <h1>Art robot测试页面-商务水牌</h1>
-<form name="form" method="get" enctype="multipart/form-data" action="export.php" >
+<form name="form" method="post" enctype="multipart/form-data">
     <h3>设置</h3>
     <p>标题:<input type="text" name="text" value="会议名称"></p>
     <p>标题:<input type="text" name="text2" value="会议名称第二行"></p>
@@ -15,7 +63,24 @@
     <p>会议时间（英文）:<input type="text" name="date-en" value="January 1 2016"></p>
     <p>会议地点:<input type="text" name="where" value="会议室1"></p>
     <p>会议地点（英文）:<input type="text" name="where-en" value="room 1"></p>
-    <p><input type="submit" value="提交"></p>
+    <p><input type="submit" name="submit" value="提交"></p>
 </form>
+<?php
+if(@$_POST['submit']) {
+    ?>
+    <h3>输出预览</h3>
+    <p><img src="export.php"></p>
+    <h3>调试输出</h3>
+    <p>
+        模板json数据
+        <textarea name="textarea" rows="10" id="textarea"><?php echo $jsontext; ?></textarea>
+        处理后数据结构
+        <textarea name="textarea" rows="10" id="textarea"><?php echo print_r($json); ?></textarea>
+        生成json数据
+        <textarea name="textarea" rows="10" id="textarea"><?php echo $jsonrun; ?></textarea>
+    </p>
+    <?php
+}
+?>
 </body>
 </html>
