07bf2ed973ec98040eab55f9c6e23191eb2d94a2
俞亦键 <yjbeetle@gmail.com>
2016-06-07 23:18:24 +0800

    缓存问题解决  原因session start

--- export.php
+++ export.php
@@ -7,13 +7,15 @@
  */
 //init
 error_reporting(0);//禁用错误报告
-session_start();
 
 //禁用缓存
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . 'GMT');
 header('Cache-Control: no-cache, must-revalidate');
 header('Pragma: no-cache');
+
+//session init
+session_start();
 
 //常量
 $TMPDIR=dirname(__FILE__).'/tmp';
