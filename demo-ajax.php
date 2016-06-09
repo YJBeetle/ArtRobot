6d850e2cdb98b54b4eb9a53286822513a1164585
俞亦键 <yjbeetle@gmail.com>
2016-06-09 15:15:15 +0800

    ajax返回json

--- demo-ajax.php
+++ demo-ajax.php
@@ -6,8 +6,11 @@
  * Time: 下午6:38
  */
 
+
 //init
 session_start();
+$return=array();
+$return['status']=0;
 
 //常量
 //$TMPDIR=sys_get_temp_dir();
@@ -16,40 +19,53 @@ if (!is_dir($TMPDIR)) mkdir($TMPDIR, 0777);
 
 //选中模板
 if (@$_GET['template'])
+{
     $template = $_GET['template'];
-else
-{
-    echo '未指定模板';
-    exit(1);
-}
 
-//设置工作目录
-$dir = "template/" . $template;
-if(!is_dir($dir))
-{
-    echo '模板不存在';
-    exit(2);
-}
-chdir($dir);
-
-//读取json文件
-$filename = "data.json";
-$file = fopen($filename, "r") or die("Unable to open file!");
-$jsontext = fread($file, filesize($filename));
-fclose($file);
-
-//POST处理
-$p=array();
-$r=array();
-if(@$_POST['TEXT'])
-{
-    foreach($_POST['TEXT'] as $v)
+    //设置工作目录
+    $dir = "template/" . $template;
+    if(!is_dir($dir))
     {
-        $p[]="/\<.*".$v[0].".*\>/";
-        $r[]=$v[1];
+        $reaurn['status']=2;
+        $reaurn['message']='模板不存在';
+
+    }
+    else
+    {
+        chdir($dir);
+
+        //读取json文件
+        $filename = "data.json";
+        $file = fopen($filename, "r") or die("Unable to open file!");
+        $jsontext = fread($file, filesize($filename));
+        fclose($file);
+
+        //POST处理
+        $p=array();
+        $r=array();
+        if(@$_POST['TEXT'])
+        {
+            foreach($_POST['TEXT'] as $v)
+            {
+                $p[]="/\<.*".$v[0].".*\>/";
+                $r[]=$v[1];
+            }
+        }
+        $jsonrun=preg_replace($p,$r,$jsontext);
+        $_SESSION['jsondata']=$jsonrun;
+
+        $return['status']=0;
+        $return['message']='绘制完毕';
+        $json=json_decode($jsonrun);
+        $return['height']=$json->height;
+        $return['width']=$json->width;
+        $return['pagecount']=is_array($json->draw[0])?count($json->draw):1;
     }
 }
-$jsonrun=preg_replace($p,$r,$jsontext);
-$_SESSION['jsondata']=$jsonrun;
+else
+{
+    $return['status']=1;
+    $return['message']='未指定模板';
+}
 
-echo '绘制完毕';
+echo json_encode($return);
