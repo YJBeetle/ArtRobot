0de8cbd9b7bf1ca8a165b70f9ae38152b2cc9b35
YJBeetle <yjbeetle@gmail.com>
2019-11-04 02:07:47 +0800

    cr 现在归Component管理

--- src/ComponentRepeat.cpp
+++ src/ComponentRepeat.cpp
@@ -9,11 +9,6 @@ ComponentRepeat::ComponentRepeat() // TODO
 {
     type = ComponentTypeRepeat;
 
-    cairo_t *cr = cairo_create(surface);
-
-
-
-    cairo_destroy(cr);
 }
 
 ComponentRepeat::~ComponentRepeat()
