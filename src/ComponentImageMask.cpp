38ce22e92adecb1fc8cc3d34313c756b3a6c54eb
YJBeetle <yjbeetle@gmail.com>
2019-11-04 00:10:04 +0800

    imageMask：child中的对象不应该受本身的xywhr影响。

--- src/ComponentImageMask.cpp
+++ src/ComponentImageMask.cpp
@@ -6,10 +6,12 @@
 namespace Render
 {
 
-void drawMat(cairo_t *cr,
-             double w, double h,
-             const Mat &maskImageMatRead,
-             cairo_surface_t *childSurface)
+void drawChildSurfaceUsedMask(cairo_t *cr,
+                              double x, double y,
+                              double w, double h,
+                              double r,
+                              const Mat &maskImageMatRead,
+                              cairo_surface_t *childSurface)
 {
     Mat maskImageMat;
     if (maskImageMatRead.channels() == 3)
@@ -17,36 +19,23 @@ void drawMat(cairo_t *cr,
     else
         maskImageMat = maskImageMatRead;
 
+// 解决Ai的蒙版兼容性问题，Ai的蒙版使用的是黑白，而Acrobat中使用的是Alpha。
     for (int y = 0; y < maskImageMat.rows; y++)
         for (int x = 0; x < maskImageMat.cols; x++)
         {
             auto &p = maskImageMat.at<Vec4b>(y, x);
-            if (p[3] == 0xFF)
+            if (p[3] == 0xFF) // 优先使用Alpha
                 p[0] = p[1] = p[2] = p[3] = (p[2] * 30 + p[1] * 59 + p[0] * 11) / 100;
             else
                 p[0] = p[1] = p[2] = p[3];
         }
     // imwrite("Debug.png", maskImageMat); // Debug
 
-    cairo_surface_t *maskImageSurface = cairo_image_surface_create_for_data(maskImageMat.data,
-                                                                            CAIRO_FORMAT_ARGB32,
-                                                                            maskImageMat.cols,
-                                                                            maskImageMat.rows,
-                                                                            maskImageMat.step);
-
-    cairo_surface_t *maskSurface = cairo_recording_surface_create(CAIRO_CONTENT_COLOR_ALPHA, NULL);
-    cairo_t *maskCr = cairo_create(maskSurface);
-    drawImageSurface(maskCr,
-                     w, h,
-                     maskImageSurface);
+    ComponentImage maskImage(x, y, w, h, r, maskImageMat);
 
     cairo_set_source_surface(cr, childSurface, 0.0, 0.0);
-    cairo_mask_surface(cr, maskSurface, 0, 0);
+    cairo_mask_surface(cr, maskImage.getSurface(), 0, 0);
     cairo_fill(cr);
-
-    cairo_surface_destroy(maskImageSurface); // 回收
-    cairo_destroy(maskCr);
-    cairo_surface_destroy(maskSurface);
 }
 
 ComponentImageMask::ComponentImageMask(double x, double y,
@@ -54,15 +43,17 @@ ComponentImageMask::ComponentImageMask(double x, double y,
                                        double r,
                                        const string &maskImageFilePath,
                                        cairo_surface_t *childSurface)
-    : Component(x, y, w, h, r)
+    : Component()
 {
     type = ComponentTypeImageMask;
 
     auto maskImageMatRead = imread(maskImageFilePath, IMREAD_UNCHANGED);
-    drawMat(cr,
-            w, h,
-            maskImageMatRead,
-            childSurface);
+    drawChildSurfaceUsedMask(cr,
+                             x, y,
+                             w, h,
+                             r,
+                             maskImageMatRead,
+                             childSurface);
 }
 
 ComponentImageMask::ComponentImageMask(double x, double y,
@@ -70,14 +61,16 @@ ComponentImageMask::ComponentImageMask(double x, double y,
                                        double r,
                                        const Mat &maskImageMatRead,
                                        cairo_surface_t *childSurface)
-    : Component(x, y, w, h, r)
+    : Component()
 {
     type = ComponentTypeImageMask;
 
-    drawMat(cr,
-            w, h,
-            maskImageMatRead,
-            childSurface);
+    drawChildSurfaceUsedMask(cr,
+                             x, y,
+                             w, h,
+                             r,
+                             maskImageMatRead,
+                             childSurface);
 }
 
 ComponentImageMask::~ComponentImageMask()
