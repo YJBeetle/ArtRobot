26e3cdd2cc3c80cf9e0d87b035ba7de3bf851f28
YJBeetle <yjbeetle@gmail.com>
2019-12-03 04:29:20 +0800

    fix: 二次移动圆角方形xy

--- src/Component/RectangleRound.cpp
+++ src/Component/RectangleRound.cpp
@@ -18,22 +18,22 @@ RectangleRound::RectangleRound(std::string __name,
 {
     cairo_set_source_rgba(cr, color.red(), color.green(), color.blue(), color.alpha());
 
-    cairo_move_to(cr, x + angleTL, y);
-    cairo_line_to(cr, x + w - angleTL, y);
+    cairo_move_to(cr, angleTL, 0);
+    cairo_line_to(cr, w - angleTL, 0);
 
-    cairo_move_to(cr, x + w, y + angleTR);
-    cairo_line_to(cr, x + w, y + h - angleTR);
+    cairo_move_to(cr, w, angleTR);
+    cairo_line_to(cr, w, h - angleTR);
 
-    cairo_move_to(cr, x + w - angleBR, y + h);
-    cairo_line_to(cr, x + angleBR, y + h);
+    cairo_move_to(cr, w - angleBR, h);
+    cairo_line_to(cr, angleBR, h);
 
-    cairo_move_to(cr, x, y + h - angleBL);
-    cairo_line_to(cr, x, y + angleBL);
+    cairo_move_to(cr, 0, h - angleBL);
+    cairo_line_to(cr, 0, angleBL);
 
-    cairo_arc(cr, x + angleTL, y + angleTL, angleTL, M_PI, 3 * M_PI / 2.0);
-    cairo_arc(cr, x + w - angleTR, y + angleTR, angleTR, 3 * M_PI / 2, 2 * M_PI);
-    cairo_arc(cr, x + w - angleBR, y + h - angleBR, angleBR, 0, M_PI / 2);
-    cairo_arc(cr, x + angleBL, y + h - angleBL, angleBL, M_PI / 2, M_PI);
+    cairo_arc(cr, angleTL, angleTL, angleTL, M_PI, 3 * M_PI / 2.0);
+    cairo_arc(cr, w - angleTR, angleTR, angleTR, 3 * M_PI / 2, 2 * M_PI);
+    cairo_arc(cr, w - angleBR, h - angleBR, angleBR, 0, M_PI / 2);
+    cairo_arc(cr, angleBL, h - angleBL, angleBL, M_PI / 2, M_PI);
 
     cairo_fill(cr);
 }
