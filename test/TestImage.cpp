087ec4601faef2345c8a10530faae5cc936cb8f9
YJBeetle <yjbeetle@gmail.com>
2022-11-03 15:01:00 +0800

    大写

--- test/TestImage.cpp
+++ test/TestImage.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
         auto img = ArtRobot::Component::Image::fromFile("img", 0, 0, 512, 512, 0, "img.jpg");
         ArtRobot::Renderer renderer(ArtRobot::OutputTypePng, 512, 512, ArtRobot::Renderer::PX, 72); // 渲染png
         renderer.render(img->getSurface());
-        renderer.saveToFile("testImageResult-fromFile.png");
+        renderer.saveToFile("TestImage-Result-fromFile.png");
     }
 
     // fromFileByCV
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
         auto img = ArtRobot::Component::Image::fromFileByCV("img", 0, 0, 512, 512, 0, "img.jpg");
         ArtRobot::Renderer renderer(ArtRobot::OutputTypePng, 512, 512, ArtRobot::Renderer::PX, 72); // 渲染png
         renderer.render(img->getSurface());
-        renderer.saveToFile("testImageResult-fromFileByCV.png");
+        renderer.saveToFile("TestImage-Result-fromFileByCV.png");
     }
 
     return 0;
