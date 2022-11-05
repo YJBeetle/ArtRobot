f2f4da937247bd19ceab6fc138f97b6d09e6d176
YJBeetle <yjbeetle@gmail.com>
2022-11-06 00:24:22 +0800

    Anchor test

--- test/Test.cpp
+++ test/Test.cpp
@@ -29,6 +29,26 @@ int main(int argc, char *argv[]) {
         renderer.render(c.getSurface());
         renderer.saveToFile("Test-Result-Scale.png");
     }
+    // Anchor
+    {
+        ArtRobot::Component::Circle cl[]={
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::LT}, "red"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::CT}, "green"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::RT}, "blue"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::LC}, "yellow"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::CC}, "purple"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::RC}, "lime"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::LD}, "teal"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::CD}, "aqua"),
+                ArtRobot::Component::Circle("Circle", 200, 200, {.anchor=ArtRobot::Component::Transform::Anchor::RD}, "maroon"),
+        };
+        auto g = ArtRobot::Component::Group("Group", {.x=200, .y=200});
+        for (const auto &c: cl)
+            g.addChild(c);
+        ArtRobot::Renderer renderer(ArtRobot::OutputTypePng, 400, 400, ArtRobot::Renderer::PX, 72);
+        renderer.render(g.getSurface());
+        renderer.saveToFile("Test-Result-Anchor.png");
+    }
 
     return 0;
 }
