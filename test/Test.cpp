f6446e61357456314a654fb9244a46596cd31029
YJBeetle <yjbeetle@gmail.com>
2022-11-06 00:17:21 +0800

    Scale test

--- test/Test.cpp
+++ test/Test.cpp
@@ -22,6 +22,13 @@ int main(int argc, char *argv[]) {
         renderer.render(c.getSurface());
         renderer.saveToFile("Test-Result-Circle.png");
     }
+    // Scale
+    {
+        auto c = ArtRobot::Component::RectangleRound("RectangleRound", 200, 200, {.x=256, .y=256, .scaleX = .5, .scaleY=2.}, 10, 20, 30, 40, "red");
+        ArtRobot::Renderer renderer(ArtRobot::OutputTypePng, 512, 512, ArtRobot::Renderer::PX, 72);
+        renderer.render(c.getSurface());
+        renderer.saveToFile("Test-Result-Scale.png");
+    }
 
     return 0;
 }
