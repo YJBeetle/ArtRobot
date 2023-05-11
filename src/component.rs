4570d238e4105b00235b80ca9927568544843492
YJBeetle <yjbeetle@gmail.com>
2023-05-11 21:47:21 +0800

    base 的构造加Option

--- src/component.rs
+++ src/component.rs
@@ -10,9 +10,11 @@ struct Base {
 }
 
 impl Base {
-    fn new(property: Property, transform: Transform) -> Self {
+    fn new(property: Option<Property>, transform: Option<Transform>) -> Self {
         let surface = RecordingSurface::create(cairo::Content::Alpha, None).unwrap();
         let cr = Context::new(&surface).unwrap();
+        let property = property.unwrap_or_default();
+        let transform = transform.unwrap_or_default();
         let ret = Self {
             surface,
             cr,
@@ -22,6 +24,7 @@ impl Base {
         ret.transform.apply(&ret.property, &ret.cr);
         ret
     }
+
     fn surface(&self) -> &RecordingSurface { &self.surface }
 }
 
@@ -37,7 +40,7 @@ pub struct Circle {
 
 impl Circle {
     pub fn new(name: String, transform: Transform, width: f64, height: f64, color: Color) -> Self {
-        let base = Base::new(Property { name, width, height }, transform);
+        let base = Base::new(Some(Property { name, width, height }), Some(transform));
         base.cr.set_source_rgba(color.r(), color.g(), color.b(), color.a());
         base.cr.scale(width / 2., height / 2.);
         base.cr.arc(1., 1., 1., 0., 2. * PI);
