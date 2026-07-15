2542cd05c41d6dded913fdefb94814e94d595e2b
YJBeetle <YJBeetle@gmail.com>
2026-07-15 21:28:30 +0800

    chore(renderer): 清理过时变换待办

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -113,7 +113,7 @@ namespace ArtRobot {
         cairo_surface_destroy(surface); //回收介质
     }
 
-    void Renderer::render(cairo_surface_t *__surface) { // todo 传入base 应用变换
+    void Renderer::render(cairo_surface_t *__surface) {
         cairo_set_source_surface(cr, __surface, 0.0, 0.0);
         cairo_paint(cr);
 
